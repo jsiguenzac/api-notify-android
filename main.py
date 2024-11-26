ddd68e80ad32199a5e4740cbb8a5710464b3130e
jsiguenzac <joelsantiago.pes@gmail.com>
2024-11-25 20:36:04 -0500

    Add variables entorno for credentials

--- main.py
+++ main.py
@@ -20,8 +20,35 @@ app.add_middleware(
 )
 
 # Inicializar Firebase
-try:
+""" try:
     cred = credentials.Certificate('credentials.json')  # Ruta al archivo de credenciales
+    initialize_app(cred)
+    db = firestore.client()
+    print("Firebase inicializado correctamente.")
+except Exception as e:
+    print(f"Error al inicializar Firebase: {e}")
+    db = None """
+    
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+try:
+    # Usa las variables de entorno para crear las credenciales
+    cred_dict = {
+        "type": "service_account",
+        "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+        "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+        "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace("\\n", "\n"),
+        "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+        "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+        "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+        "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+        "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+        "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
+        "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN')
+    }
+    cred = credentials.Certificate(cred_dict)
     initialize_app(cred)
     db = firestore.client()
     print("Firebase inicializado correctamente.")
